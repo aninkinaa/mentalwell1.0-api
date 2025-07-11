e1791d1d8351a50ecf7a3843148d397aa58bfefc
aninkinaa <anin.kinarya@gmail.com>
2025-07-11 21:12:42 +0700

    add validation for file upload to only allow JPG and PNG formats

--- config/uploadFile.js
+++ config/uploadFile.js
@@ -1,35 +1,46 @@
 const { supabase } = require('./database');
 const { nanoid } = require('nanoid');
 const path = require('path');
+const { ValidationError } = require('../utils/errors');
 
 const uploadPhotoToSupabase = async ({ file, folder = 'profile_images', prefix = '' }) => {
-    if (!file?.hapi?.filename || !file?._data) {
-      throw new Error('File tidak valid. Pastikan Anda mengunggah file yang benar.');
-    }
-  
-    const fileExt = path.extname(file.hapi.filename);
-    const randomString = nanoid(4);
-    const fileName = `${prefix}${prefix ? '-' : ''}${randomString}${fileExt}`;
-    const filePath = `${folder}/${fileName}`;
-  
-    const { error: uploadError } = await supabase.storage
-      .from('mentalwell-bucket')
-      .upload(filePath, file._data, {
-        contentType: file.hapi.headers['content-type'],
-        upsert: true,
-      });
-  
-    if (uploadError) throw new Error(`Gagal upload foto: ${uploadError.message}`);
-  
-    const { data } = supabase.storage
-      .from('mentalwell-bucket')
-      .getPublicUrl(filePath);
-  
-    return {
-      success: true,
-      url: data.publicUrl,
-    };
+  if (!file?.hapi?.filename || !file?._data) {
+    throw new ValidationError('File tidak valid. Silakan unggah ulang.');
+  }
+
+  const contentType = file.hapi.headers['content-type'];
+  const allowedMimeTypes = ['image/jpeg', 'image/png'];
+
+  if (!allowedMimeTypes.includes(contentType)) {
+    throw new ValidationError('Format file tidak didukung. Silakan unggah JPG atau PNG');
+  }
+
+  const fileExt = path.extname(file.hapi.filename);
+  const randomString = nanoid(4);
+  const fileName = `${prefix}${prefix ? '-' : ''}${randomString}${fileExt}`;
+  const filePath = `${folder}/${fileName}`;
+
+  const { error: uploadError } = await supabase.storage
+    .from('mentalwell-bucket')
+    .upload(filePath, file._data, {
+      contentType,
+      upsert: true,
+    });
+
+  if (uploadError) {
+    throw new Error(`Gagal upload foto: ${uploadError.message}`);
+  }
+
+  const { data } = supabase.storage
+    .from('mentalwell-bucket')
+    .getPublicUrl(filePath);
+
+  return {
+    success: true,
+    url: data.publicUrl,
   };
+};
+
   
 
 module.exports = { uploadPhotoToSupabase };
